b6fbcc384452eaf8aaf6880db6ff70077b5b1933
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-17 09:16:13 +0900

    Add tests for handlers (#17)

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type postData struct {
+	key   string
+	value string
+}
+
+var theTests = []struct {
+	name               string
+	url                string
+	method             string
+	param              []postData
+	expectedstatusCode int
+}{
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"gq", "/generals-quarters", "GET", []postData{}, http.StatusOK},
+	{"ms", "/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"ms", "/make-reservation", "GET", []postData{}, http.StatusOK},
+}
+
+func TestHandlers(t *testing.T) {
+	routes := getRoutes()
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTests {
+		if e.method == "GET" {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedstatusCode {
+				t.Errorf("for %s, expected %d but god %d", e.name, e.expectedstatusCode, resp.StatusCode)
+			}
+		} else {
+
+		}
+	}
+}
