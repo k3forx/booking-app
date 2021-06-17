e1bd22fddbc8d3359127fc26294d887b91fdfe82
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-17 21:51:06 +0900

    Add unit tests for render.go (#19)

---
+++ internal/render/setup_test.go
@@ -0,0 +1,50 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/k3forx/booking-app/internal/config"
+	"github.com/k3forx/booking-app/internal/models"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// Change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
+
+type myWriter struct{}
+
+func (w *myWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (w *myWriter) WriteHeader(i int) {}
+
+func (w *myWriter) Write(b []byte) (int, error) {
+	length := len(b)
+	return length, nil
+}
