8bbfa246e7d7d7e916635ca510ead30b628409d9
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-20 22:17:54 +0900

    Connecting to the database and adding the SQL connection to our Repository (#30)

---
+++ internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,31 @@
+package dbrepo
+
+import (
+	"database/sql"
+
+	"github.com/k3forx/booking-app/internal/config"
+	"github.com/k3forx/booking-app/internal/repository"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostgresRepo(a *config.AppConfig, conn *sql.DB) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
+
+func NewTestingRepo(a *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: a,
+	}
+}
