fbea3ad76d9e1665736f7c7543cc3789663b5377
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-21 21:24:43 +0900

    Create function to insert reservation (#33)

---
+++ postgres/sample_sql/insert_restrictions.sql
@@ -0,0 +1,1 @@
+INSERT INTO restrictions (id, restriction_name, created_at, updated_at) VALUES (1, 'restriction_1', now(), now());
---
+++ postgres/sample_sql/insert_rooms.sql
@@ -0,0 +1,1 @@
+INSERT INTO rooms (id, room_name, created_at, updated_at) VALUES (1, 'test_room1', now(), now());
