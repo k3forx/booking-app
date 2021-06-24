bc3ad7791edd0e88a8c96338fabdbe15f0e968cc
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-24 18:02:49 +0900

    Revert "Add templates for admin pages (#48)" (#56)
    This reverts commit d9fad65ea8e8649ff1a28a985ca0b5d4d24259d8.

--- postgres/sample_sql/insert_user.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (first_name, last_name, email, password, access_level, created_at, updated_at) VALUES ('test', 'test', 'admin@admin.com', '$2a$12$zdB4.C7CmbdMQkp5GTR4l.n5UDt3o831pjLTmD8WBN05Gk14shFau', 3, now(), now());
